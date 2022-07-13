ee9a3ae15963c2c3cb764ca77e35fc3bab5681b7
Chathurika De Silva <naleenperera68@gmail.com>
2022-07-13 12:30:46 +0530

    Add project to repo

---
+++ README.md
@@ -0,0 +1,21 @@
+# README
+This project contains a sample that demonstrates invoking a rest api and validating three test cases.
+
+### Technologies used
+- Testng
+- Restassured
+- Maven
+- Java
+
+### Project description
+This calls a REST endpoint and validates the follwoing criteria
+
+Name = "Carbon credits"
+CanRelist = true
+The Promotions element with Name = "Gallery" has a Description that contains the text "Good position in category"
+
+### Pre-requsists to run the test
+- A device with Maven installed
+- A device with git installed
+
+### Steps to run
---
+++ src/test/java/SampleAPITest.java
@@ -0,0 +1,61 @@
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import io.restassured.response.Response;
+
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static io.restassured.RestAssured.given;
+
+public class SampleAPITest {
+    Response response;
+    Logger LOGGER = Logger.getLogger(SampleAPITest.class.getName());
+
+    //This before test method is used to call the API endpoint and assign the response to a variable of type Response
+    @BeforeTest
+    public void callEndpoint() {
+        try {
+            LOGGER.log(Level.INFO, "Invoking API endpoint");
+            response = given()
+                    .when()
+                    .get("https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false")
+                    .then().statusCode(200).extract().response();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Exception occured" + "--" + e.getMessage(), e);
+        }
+    }
+
+    //This test method is used to verify whether the json payload's Name element consists of string "Carbon credits"
+    @Test(description = "Retrieve and assert Name")
+    public void testNameElement() {
+        LOGGER.log(Level.INFO, "Executing test method - testNameElement");
+        Assert.assertEquals(response.jsonPath().get("Name"), "Carbon credits");
+    }
+
+    //This test method is used to verify whether the json payload's CanRelist element consists of string "true"
+    @Test(description = "Retrieve and assert CanRelist")
+    public void testCanRelistElement() {
+        LOGGER.log(Level.INFO, "Executing test method - testCanRelistElement");
+        Assert.assertTrue(response.jsonPath().get("CanRelist"), "true");
+    }
+
+    //This test method is used to verify whether the Promotions array's objects consists of an element named Gallery
+    //and thereafter checks the description element to consist string "Good position in category"
+    @Test(description = "Retrieve and assert description of Gallery promotion item")
+    public void testPromotionsForString() {
+        LOGGER.log(Level.INFO, "Executing test method - testPromotionsForString");
+        JsonPath jsonPathEval = response.jsonPath();
+        int sizeofPromotions = jsonPathEval.getInt("Promotions.size()");
+        for (int i = 0; i < sizeofPromotions; i++) {
+            Map<String, String> promotions = jsonPathEval.getMap("Promotions[" + i + "]");
+            String val1 = promotions.get("Name");
+            if (val1.equalsIgnoreCase("Gallery")) {
+                Assert.assertEquals(promotions.get("Description"), "Good position in category");
+            }
+
+        }
+    }
+
+}
